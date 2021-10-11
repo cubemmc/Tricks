49b87463f889600c24d163a3a83196cd2a436b34
xumeng.zuo <https://code.cnjiang.com/canada/oz-store-admin>
2021-10-11 16:32:38 +0800

    fix(test):

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-multiple-empty-lines': 0,
-    'eol-last': 0,
-    'no-trailing-spaces': 0,
-    'no-unused-vars': 0,
-    'vue/no-unused-vars': 0,
-    'eqeqeq': 0,
-    'no-useless-return': 0
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-multiple-empty-lines": 0,
+    "eol-last": 0,
+    "no-trailing-spaces": 0,
+    "no-unused-vars": "warn",
+    "vue/no-unused-vars": "warn",
+    eqeqeq: 0,
+    "no-useless-return": 0,
+    quotes: [1, "single"],
+    singleQuote: true,
+    semi: false,
+    // "space-before-function-paren": [0, "always"],
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    // parser: 'babel-eslint'
+  },
+};
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,24 +1,24 @@
-import { randomColor } from '@/utils/common'
+import { randomColor } from "@/utils/common";
 export default {
-  name: 'home',
-  data () {
+  name: "home",
+  data() {
     return {
-      cssList: []
-    }
+      cssList: [],
+    };
   },
-  mounted () {
-    this.getList()
+  mounted() {
+    this.getList();
   },
   methods: {
-    getList () {
+    getList() {
       this.cssList = [
         {
           id: 7,
-          cover: '5.jpg',
-          title: 'css动画之3D旋转相册',
-          desc: 'css动画之3D旋转相册',
+          cover: "5.jpg",
+          title: "css动画之3D旋转相册",
+          desc: "css动画之3D旋转相册",
           isHide: false,
-          routeName: 'cssRotatePhotos'
+          routeName: "cssRotatePhotos",
         },
         // {
         //   id: 6,
@@ -30,11 +30,11 @@ export default {
         // },
         {
           id: 5,
-          cover: '5.jpg',
-          title: 'css特效之rotate',
-          desc: 'css特效之rotate',
+          cover: "5.jpg",
+          title: "css特效之rotate",
+          desc: "css特效之rotate",
           isHide: false,
-          routeName: 'cssRotate'
+          routeName: "cssRotate",
         },
         // {
         //   id: 4,
@@ -43,49 +43,49 @@ export default {
         //   desc: 'css特效之手风琴(无序列表)',
         //   isHide: true,
         //   routeName: 'according'
-        // },        
+        // },
         {
           id: 1,
-          cover: '3.jpg',
-          title: 'css特效之css进度条',
-          desc: 'css特效之css进度条',
+          cover: "3.jpg",
+          title: "css特效之css进度条",
+          desc: "css特效之css进度条",
           isHide: false,
-          routeName: 'cssProgress'
-        },        
+          routeName: "cssProgress",
+        },
         {
           id: 3,
-          cover: '2.jpg',
-          title: 'css动画之cssLoading',
-          desc: 'css动画之cssLoading',
+          cover: "2.jpg",
+          title: "css动画之cssLoading",
+          desc: "css动画之cssLoading",
           isHide: false,
-          routeName: 'cssLoading'
+          routeName: "cssLoading",
         },
         {
           id: 2,
-          cover: '1.jpg',
-          title: 'css动画之css方块跳动',
-          desc: 'css动画之css方块跳动',
+          cover: "1.jpg",
+          title: "css动画之css方块跳动",
+          desc: "css动画之css方块跳动",
           isHide: false,
-          routeName: 'blockJump'
+          routeName: "blockJump",
         },
         {
           id: 2,
-          cover: '1.jpg',
-          title: 'css实现半透明边框',
-          desc: 'css实现半透明边框',
+          cover: "1.jpg",
+          title: "css实现半透明边框",
+          desc: "css实现半透明边框",
           isHide: false,
-          routeName: 'blockJump'
-        }
-      ]
+          routeName: "blockJumptest",
+        },
+      ];
     },
-    triggerToCssLink (name) {
+    triggerToCssLink(name) {
       if (name) {
         this.$router.push({
-          name
-        })
+          name,
+        });
       } else {
-        alert('敬请期待！')
+        alert("敬请期待！");
       }
-    }
-  }
-}
+    },
+  },
+};
